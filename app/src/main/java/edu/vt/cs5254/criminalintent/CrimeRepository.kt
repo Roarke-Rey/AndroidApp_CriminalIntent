ce0e8ca8379089697d3b4a424f6e9c36aea223d8
Roarke-Rey <shreyas992010@gmail.com>
2023-04-08 20:48:46 -0400

    Added the photo intent to support photo from camera in crime detail fragment

--- app/src/main/java/edu/vt/cs5254/criminalintent/CrimeRepository.kt
+++ app/src/main/java/edu/vt/cs5254/criminalintent/CrimeRepository.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import androidx.room.Room
 import edu.vt.cs5254.criminalintent.database.CrimeDatabase
 import edu.vt.cs5254.criminalintent.database.migration_1_2
+import edu.vt.cs5254.criminalintent.database.migration_2_3
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.flow.Flow
@@ -23,7 +24,7 @@ class CrimeRepository private constructor(
             DATABASE_NAME
         )
 //        .createFromAsset(DATABASE_NAME)           // Doesnt populate the crime database anymroe
-        .addMigrations(migration_1_2)
+        .addMigrations(migration_1_2, migration_2_3)
         .build()
 
     fun getCrimes(): Flow<List<Crime>> = database.crimeDao().getCrimes()
